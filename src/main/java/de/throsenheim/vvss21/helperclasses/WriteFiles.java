99f7e8a106989ec83f84b46c4cfe30df6c9cc072
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-03-25 22:15:14 +0100

    Added Method to create a config file

--- src/main/java/de/throsenheim/vvss21/helperclasses/WriteFiles.java
+++ src/main/java/de/throsenheim/vvss21/helperclasses/WriteFiles.java
@@ -1,4 +1,27 @@
 package de.throsenheim.vvss21.helperclasses;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * For writing new files witch are predefined
+ * <p>e.g. config file
+ */
 public class WriteFiles {
+
+    public WriteFiles() {
+    }
+
+    public synchronized void createConfig(File file){
+        try {
+            System.out.println("Creating new File at " + file.getAbsolutePath());
+            FileWriter writer = new FileWriter(file);
+            writer.write("This is the config file for alexanderasbeck\n"+
+                    "Logaddress= \n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
