e05d4a6ca025d66a7eb54ff5cc1ef094fbd70388
studasbeal4195 <alexander.asbeck@stud.th-rosenheim.de>
2021-04-01 17:21:30 +0200

    Revert "Merge branch '6-commandline' into 'master'"
    This reverts merge request !2

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        HelloWorld();
+    }
+
+    public void HelloWorld(){
+        System.out.println("Hello World");
+    }
+}
