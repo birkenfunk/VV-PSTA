2e5a63c74138dc21ed5c0c352afab703303f2f0f
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-02 14:37:35 +0200

    Adding Tests for ReadFile and WriteFile

---
+++ src/test/java/de/throsenheim/vvss21/helperclasses/readers/ReadFileTest.java
@@ -0,0 +1,12 @@
+package de.throsenheim.vvss21.helperclasses.readers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReadFileTest {
+
+    @Test
+    void readFile() {
+    }
+}
---
+++ src/test/java/de/throsenheim/vvss21/helperclasses/writers/WriteFilesTest.java
@@ -0,0 +1,17 @@
+package de.throsenheim.vvss21.helperclasses.writers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WriteFilesTest {
+
+    @Test
+    void createConfig() {
+
+    }
+
+    @Test
+    void writeFile() {
+    }
+}
