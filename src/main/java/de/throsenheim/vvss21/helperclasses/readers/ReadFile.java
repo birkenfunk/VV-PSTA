bc50450d3e65504e6c0de9e3bbd97f72fdd78fd2
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-02 13:46:31 +0200

    Added a static method & private constructor
    Static method for reading any file and returning it's content
    Made a private constructor so no instance of the class can be created

--- src/main/java/de/throsenheim/vvss21/helperclasses/readers/ReadFile.java
+++ src/main/java/de/throsenheim/vvss21/helperclasses/readers/ReadFile.java
@@ -1,4 +1,52 @@
 package de.throsenheim.vvss21.helperclasses.readers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * A Class that scans a File
+ * @author Alexander Asbeck
+ * @version 1.0.0
+ */
 public class ReadFile {
+
+    private static final Logger LOGGER = LogManager.getLogger(ReadFile.class);
+
+    /**
+     * Private constructor due to no need of a new instance of a class
+     */
+    private ReadFile(){
+    }
+
+    /**
+     * A method for reading a File
+     * @param file File that should be read
+     * @return Gives a List with the content of the File each Line ist a entry in the List
+     */
+    public static List<String> readFile(File file){
+        List<String> out = new LinkedList<>();
+        LOGGER.debug("Try to read the file: " + file.getAbsolutePath());
+        if (!file.exists()){
+            LOGGER.debug("Could not found the file: " + file.getAbsolutePath());
+            return out;
+        }
+        try (Scanner fileScanner = new Scanner(file)){
+            String line;
+            while (fileScanner.hasNext()){
+                line=fileScanner.nextLine();
+                out.add(line);
+            }
+            LOGGER.debug("File reading was successful");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            LOGGER.error(e);
+        }
+        return out;
+    }
 }
