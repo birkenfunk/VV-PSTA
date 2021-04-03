111f91b0ed333055b00f1c1f346db22148c6c30d
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-03 22:15:10 +0200

    Added Test Class for Main

---
+++ src/test/java/de/throsenheim/vvss21/MainTest.java
@@ -0,0 +1,12 @@
+package de.throsenheim.vvss21;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+    }
+}
