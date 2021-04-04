bd7d4538a87c677da05c2d50990d4f8b2fd6c266
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-04 13:01:48 +0200

    Updated sonarcube.sh to the right key

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,1 +1,1 @@
-./gradlew sonarqube   -Dsonar.projectKey=VVPSTA   -Dsonar.host.url=http://birkenfunk.de:9000   -Dsonar.login=ecf038ba90a44902d3b419f73b5187e50a299435
+./gradlew sonarqube -Dsonar.projectKey=VVPSTA -Dsonar.host.url=http://birkenfunk.de:9000 -Dsonar.login=08cf5e193cd203639be96dfc2122621dfeb40493
