04dbfdfed786f290fc9c91b31d426e433b5f0442
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-12 18:00:02 +0200

    Added a Logger and a way to create a new connectors and close all connections

--- src/main/java/de/throsenheim/vvss21/tcpserver/Server.java
+++ src/main/java/de/throsenheim/vvss21/tcpserver/Server.java
@@ -1,12 +1,23 @@
 package de.throsenheim.vvss21.tcpserver;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.LinkedList;
 
 public class Server implements Runnable{
 
     private static final int PORT = 1024;
     private static boolean run = true;
+    private static Server server;
+    private static final Logger LOGGER = LogManager.getLogger(Server.class);
+    private static final LinkedList<Connector> connectors = new LinkedList<>();
+    private static ServerSocket serverSocket;
+
+    private Server() {
+    }
 
     /**
      * When an object implementing interface <code>Runnable</code> is used
@@ -21,17 +32,46 @@ public class Server implements Runnable{
      */
     @Override
     public void run() {
-        try (ServerSocket server = new ServerSocket(PORT);){
+        try {
+            setServer();
             while (run){
-                new Connector(server.accept());
+                connectors.add(new Connector(serverSocket.accept()));
+                Thread connector = new Thread(connectors.getLast());
+                connector.start();
+                LOGGER.info("Connection");
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error(e);
+        } finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                LOGGER.error(e);
+            }
         }
 
     }
 
+    private static void setServer() throws IOException {
+        serverSocket = new ServerSocket(PORT);
+    }
+
     public static void stop(){
         run = false;
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            LOGGER.error(e);
+        }
+        while (!connectors.isEmpty()){
+            connectors.remove().stop();
+        }
+    }
+
+    public static Server getServerSocket() {
+        if(serverSocket == null){
+            server = new Server();
+        }
+        return server;
     }
 }
