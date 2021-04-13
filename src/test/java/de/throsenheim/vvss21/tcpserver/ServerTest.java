72b4dd5d6aa832c3751a4cf8239cf4f0ca35c8c1
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-13 13:51:14 +0200

    Adding Test for server class

---
+++ src/test/java/de/throsenheim/vvss21/tcpserver/ServerTest.java
@@ -0,0 +1,15 @@
+package de.throsenheim.vvss21.tcpserver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ServerTest {
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void stop() {
+    }
+}
