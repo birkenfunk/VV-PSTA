46443d7a6e01770d3b23a3ac4b5498b36e9de6cb
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-13 23:34:13 +0200

    Adding a class that contains all measurements

--- src/main/java/de/throsenheim/vvss21/measurement/MeasurementList.java
+++ src/main/java/de/throsenheim/vvss21/measurement/MeasurementList.java
@@ -1,2 +1,13 @@
-package de.throsenheim.vvss21.measurement;public class MeasurementList {
+package de.throsenheim.vvss21.measurement;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class MeasurementList {
+    List<Measurement> measurements;
+
+    public MeasurementList(@JsonProperty("measurements") List<Measurement> measurements) {
+        this.measurements = measurements;
+    }
 }
