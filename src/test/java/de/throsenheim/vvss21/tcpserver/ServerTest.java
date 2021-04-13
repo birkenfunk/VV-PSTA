c23b9c89a2b67cd4fe32770fbcee9198e8f6f3f7
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-13 15:37:36 +0200

    new Tests

--- src/test/java/de/throsenheim/vvss21/tcpserver/ServerTest.java
+++ src/test/java/de/throsenheim/vvss21/tcpserver/ServerTest.java
@@ -1,16 +1,29 @@
 package de.throsenheim.vvss21.tcpserver;
 
+import de.throsenheim.vvss21.Main;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ServerTest {
 
     @Test
-    void run() {
+    void run() throws Exception {
+        Main.main(null);
+        Socket socket = new Socket("localhost", 1024);
+        InputStream fromClientStream = socket.getInputStream();
+        Scanner fromClient = new Scanner(fromClientStream);
+        String output = fromClient.nextLine();
+        assertEquals("Connection successful",output);
     }
 
     @Test
-    void stop() {
+    void stop() throws Exception {
+
     }
+
 }
