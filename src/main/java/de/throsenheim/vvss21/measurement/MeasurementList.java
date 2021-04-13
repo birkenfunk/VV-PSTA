b405def536a0aa00dde971b1012b70802e4533e5
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-14 00:06:22 +0200

    Added a Logger and a method to add a measurement file to the list and save the file as a json

--- src/main/java/de/throsenheim/vvss21/measurement/MeasurementList.java
+++ src/main/java/de/throsenheim/vvss21/measurement/MeasurementList.java
@@ -1,19 +1,43 @@
 package de.throsenheim.vvss21.measurement;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import de.throsenheim.vvss21.helperclasses.writers.WriteFiles;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
 public class MeasurementList {
-    private List<Measurement> measurements;
+    private final List<Measurement> measurements;
+    private static final Logger LOGGER = LogManager.getLogger(MeasurementList.class);
 
     public MeasurementList(@JsonProperty("measurements") List<Measurement> measurements) {
         this.measurements = measurements;
     }
-
+    // TODO: 13.04.21  BlockingQue
     public List<Measurement> getMeasurements() {
         return measurements;
+    }
+
+    public void add(Measurement measurement){
+        measurements.add(measurement);
+
+    }
+
+    private void saveToJsonFile(File file){
+        List<String> strings = new LinkedList<>();
+        JsonNode node = Json.toJson(this);
+        try {
+            strings.add(Json.prittyPrint(node));
+        } catch (IOException e) {
+            LOGGER.error(e);
+        }
+        WriteFiles.writeFile(file, strings, true);
     }
 
     @Override
