4a7b00cd4102d09aa165f4af204fb4306d089543
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-13 22:33:27 +0200

    Added some tests for Json

---
+++ src/test/java/de/throsenheim/vvss21/measurement/JsonTest.java
@@ -0,0 +1,15 @@
+package de.throsenheim.vvss21.measurement;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class JsonTest {
+
+    @Test
+    void parse() {
+    }
+
+    @Test
+    void fromJson() {
+    }
+}
