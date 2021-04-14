92ed490e4c23a1ba8aa7f0c7353b8c9d7da6b9b0
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-14 22:19:57 +0200

    reworked add method

--- src/main/java/de/throsenheim/vvss21/measurement/MeasurementList.java
+++ src/main/java/de/throsenheim/vvss21/measurement/MeasurementList.java
@@ -2,6 +2,8 @@ package de.throsenheim.vvss21.measurement;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
+import de.throsenheim.vvss21.Main;
+import de.throsenheim.vvss21.helperclasses.json.Json;
 import de.throsenheim.vvss21.helperclasses.writers.WriteFiles;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,25 +13,41 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
-public class MeasurementList {
+public class MeasurementList implements Runnable{
     private final List<Measurement> measurements;
     private static final Logger LOGGER = LogManager.getLogger(MeasurementList.class);
+    private final BlockingQueue<Measurement> measurementBlockingQueue = new LinkedBlockingQueue<>();
+    private boolean run = true;
 
     public MeasurementList(@JsonProperty("measurements") List<Measurement> measurements) {
         this.measurements = measurements;
     }
-    // TODO: 13.04.21  BlockingQue
+
     public List<Measurement> getMeasurements() {
         return measurements;
     }
 
     public void add(Measurement measurement){
-        measurements.add(measurement);
+        if(measurement == null){
+            return;
+        }
+        try {
+            boolean success = measurementBlockingQueue.offer(measurement);
+            if(!success){
+                LOGGER.debug("Inserting Measurement went wrong");
+            }
+        }catch (Exception e){
+            LOGGER.error(e);
+        }
+
 
     }
 
-    private void saveToJsonFile(File file){
+    private void saveToJsonFile(){
         List<String> strings = new LinkedList<>();
         JsonNode node = Json.toJson(this);
         try {
@@ -37,7 +55,8 @@ public class MeasurementList {
         } catch (IOException e) {
             LOGGER.error(e);
         }
-        WriteFiles.writeFile(file, strings, true);
+        File storeTo = new File(Main.getJsonLocation());
+        WriteFiles.writeFile(storeTo, strings, true);
     }
 
     @Override
@@ -56,8 +75,41 @@ public class MeasurementList {
         return true;
     }
 
+    private void addToMeasurement(){
+        while (run) {
+            try {
+                Measurement measurement = measurementBlockingQueue.poll(1, TimeUnit.SECONDS);
+                if (measurement != null) {
+                    measurements.add(measurement);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void stop(){
+        run = false;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(measurements);
+    }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        addToMeasurement();
     }
 }
