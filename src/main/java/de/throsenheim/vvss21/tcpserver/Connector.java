ef1e1144df293ac694c8489449e177b4de15663b
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-17 18:25:46 +0200

    refactored run methode and moved it to the outside

--- src/main/java/de/throsenheim/vvss21/tcpserver/Connector.java
+++ src/main/java/de/throsenheim/vvss21/tcpserver/Connector.java
@@ -24,19 +24,7 @@ class Connector implements Runnable{
         this.client = client;
     }
 
-    /**
-     * When an object implementing interface <code>Runnable</code> is used
-     * to create a thread, starting the thread causes the object's
-     * <code>run</code> method to be called in that separately executing
-     * thread.
-     * <p>
-     * The general contract of the method <code>run</code> is that it may
-     * take any action whatsoever.
-     *
-     * @see Thread#run()
-     */
-    @Override
-    public void run() {
+    private void connection(){
         try {
             InputStream fromClientStream = client.getInputStream();
             Scanner fromClient = new Scanner(fromClientStream);
@@ -58,6 +46,22 @@ class Connector implements Runnable{
         } catch (IOException e) {
             LOGGER.error(e);
         }
+    }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        connection();
     }
 
     public void stop(){
