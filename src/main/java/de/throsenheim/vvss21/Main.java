17d6210a6599ad5fda78d636b12191f8685bb6f0
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-26 16:45:10 +0200

    Removed config file reading and instad added properties file reading

--- src/main/java/de/throsenheim/vvss21/Main.java
+++ src/main/java/de/throsenheim/vvss21/Main.java
@@ -3,18 +3,15 @@ package de.throsenheim.vvss21;
 import com.fasterxml.jackson.databind.JsonNode;
 import de.throsenheim.vvss21.helperclasses.json.Json;
 import de.throsenheim.vvss21.helperclasses.readers.ReadFile;
-import de.throsenheim.vvss21.helperclasses.writers.WriteFiles;
 import de.throsenheim.vvss21.measurement.MeasurementList;
 import de.throsenheim.vvss21.tcpserver.Server;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * Main Class from where the program should be started
@@ -23,16 +20,14 @@ import java.util.List;
  */
 public class Main {
 
-    private File configFile = new File("alexanderasbeck.conf");
-    private static final Logger LOGGER = LogManager.getLogger(Main.class);
-    private final ReadConsole readConsole;
-    private static MeasurementList measurementList;
-    private static String jsonLocation = "data.json";
-    private static int port = 1024;
-
     public static void main(String[] args) {
         new Main(args);
     }
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+    private static MeasurementList measurementList;
+    private static String jsonLocation = "data.json";
+    private static int port = 1024;
+    private final ReadConsole readConsole;
 
     /**
      * Constructor for Main Class
@@ -40,16 +35,9 @@ public class Main {
      * @param args args given by main
      */
     public Main(String[] args) {
-        // TODO: 20.04.21 Get environment variables
         this.readConsole = new ReadConsole();
         logStartup();
-        if(args!= null && args.length>0){
-            inputComparison(args);
-        }
-        if(!configFile.exists()){
-            WriteFiles.createConfig(configFile);
-        }
-        readConf(ReadFile.readFile(configFile));
+        readProperties();
         getMeasurementList();
         Thread measurementSave  = new Thread(measurementList);
         measurementSave.start();
@@ -59,81 +47,36 @@ public class Main {
         serverThread.start();
     }
 
-    /**
-     * Method writes the start variables to the log
-     */
-    private void logStartup(){
-        String debugMsg = "Program start";
-        LOGGER.debug(debugMsg);
-        debugMsg = "Java Version: "+ System.getProperty("java.version");
-        LOGGER.debug(debugMsg);
-        debugMsg = "OS: "+ System.getProperty("os.name");
-        LOGGER.debug(debugMsg);
-        debugMsg = "OS Version: "+System.getProperty("os.version");
-        LOGGER.debug(debugMsg);
-        debugMsg = "Architecture: "+System.getProperty("os.arch");
-        LOGGER.debug(debugMsg);
-        debugMsg = "User: "+System.getProperty("user.name");
-        LOGGER.debug(debugMsg);
+    private static void readProperties(){
+        try (InputStream fileInputStream = Main.class.getClassLoader().getResourceAsStream("alexanderasbeck.properties")){
+            Properties properties = new Properties();
+            properties.load(fileInputStream);
+            String temp = properties.getProperty("JSON_FILE");
+            jsonLocation = transformProperties(temp);
+            temp = properties.getProperty("PORT");
+            temp = transformProperties(temp);
+            port = Integer.parseInt(temp);
+        } catch (IOException e) {
+            LOGGER.error(e);
+        }
 
     }
 
-    /**
-     * Reads out the Config out of a List
-     * @param list List with the config
-     */
-    private static boolean readConf(List<String> list){
-        if(list.isEmpty()){
-            LOGGER.info("No config received");
-            return false;
+    private static String transformProperties(String property){
+        if(!property.startsWith("${") && !property.endsWith("}")){
+            return property;
         }
-        String confStart = "This is the config file for alexanderasbeck";
-        if(list.remove(0).equals(confStart)){
-            for (String s: list) {
-                String[] splittedConf = s.split("=");
-                if(s.startsWith("JSON_Location") && splittedConf.length>2){
-                    jsonLocation = splittedConf[1];
-                }
-                if(s.startsWith("PORT") && splittedConf.length>2){
-                    port = Integer.parseInt(splittedConf[1]);
-                }
-                //here can other config parameter be sorted out
-            }
+        String copyProperty = property.replaceAll("[${|}]","");
+        String[] splitedProperty = copyProperty.split(":");
+        if (!splitedProperty[0].equals("env") && splitedProperty.length<3){
+            return property;
         }
-        list.add(0,confStart);
-        return true;
-    }
-
-    /**
-     * Used to see if the input parameters are correct
-     * @param input the inputs that should be compared
-     */
-    private void inputComparison(String[] input){
-        for (int i = 0; i < input.length; i++) {
-            if(input[i].equalsIgnoreCase("--conf")){
-                if(input.length >= i+2 && input[i+1].endsWith(".conf")){
-                    configFile = new File(input[i+1]);
-                    if(!configFile.exists()){
-                        WriteFiles.createConfig(configFile);
-                    }
-                }else {
-                    String debugMsg = "Use --conf [filepath]\n" +
-                            "Note that you have to enter a .conf file";
-                    LOGGER.info(debugMsg);
-                    debugMsg = "Now using default config "+ configFile.getPath();
-                    LOGGER.debug(debugMsg);
-                }
-
-            }
+        Map<String, String> env = System.getenv();
+        if(env.containsKey(splitedProperty[1])){
+            return env.get(splitedProperty[1]);
         }
-    }
 
-    /**
-     * Returns the location for the configFile
-     * @return Location for the config File
-     */
-    public File getConfigFile() {
-        return configFile;
+        return splitedProperty[2].replaceAll("[-]","");
     }
 
     /**
@@ -176,6 +119,25 @@ public class Main {
     }
 
     /**
+     * Method writes the start variables to the log
+     */
+    private void logStartup(){
+        String debugMsg = "Program start";
+        LOGGER.debug(debugMsg);
+        debugMsg = "Java Version: "+ System.getProperty("java.version");
+        LOGGER.debug(debugMsg);
+        debugMsg = "OS: "+ System.getProperty("os.name");
+        LOGGER.debug(debugMsg);
+        debugMsg = "OS Version: "+System.getProperty("os.version");
+        LOGGER.debug(debugMsg);
+        debugMsg = "Architecture: "+System.getProperty("os.arch");
+        LOGGER.debug(debugMsg);
+        debugMsg = "User: "+System.getProperty("user.name");
+        LOGGER.debug(debugMsg);
+
+    }
+
+    /**
      * Class that reads from input from the Console
      * @author Alexander Asbeck
      * @version 1.1.0
@@ -213,18 +175,6 @@ public class Main {
                 measurementList.stop();
                 Server.stop();
                 LOGGER.info("Stopped Program");
-                return;
-            }
-            String[] splittedCommand = command.split(" ");
-            if(splittedCommand[0].equalsIgnoreCase("config")){
-                if(splittedCommand.length == 2 && splittedCommand[1].endsWith(".conf")){
-                    configFile = new File(splittedCommand[1]);
-                    if(!configFile.exists()){
-                        WriteFiles.createConfig(configFile);
-                    }
-                }else {
-                    LOGGER.info("Use Command like config [filepath]\n Note that you have to enter a .conf file");
-                }
                 return;
             }
             LOGGER.info("Use help to get all commands");
