69e478311a3cc4602232101bb40e00b9089820c8
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-30 23:25:09 +0200

    Added methods and docu to the IMeaurement

--- src/main/java/de/throsenheim/vvss21/application/interfaces/IMeasurement.java
+++ src/main/java/de/throsenheim/vvss21/application/interfaces/IMeasurement.java
@@ -1,2 +1,35 @@
-package de.throsenheim.vvss21.application.interfaces;public interface IMeasurement {
+package de.throsenheim.vvss21.application.interfaces;
+
+import de.throsenheim.vvss21.measurement.Type;
+import de.throsenheim.vvss21.measurement.Unit;
+
+/**
+ * Interface for handling measurement objects
+ */
+public interface IMeasurement{
+
+    /**
+     * Getter of value
+     * @return Integer with the value of the measurement
+     */
+    public int getValue();
+
+    /**
+     * Getter of Unit
+     * @return String with the Unit of the measurement
+     */
+    public Unit getUnit();
+
+    /**
+     * Getter of Type
+     * @return String with the Type of the measurement
+     */
+    public Type getType();
+
+    /**
+     * Getter of Timestamp
+     * @return String with the Timestamp of the measurement with the pattern: 'yyyy-MM-dd HH:mm:ss.SS'
+     */
+    public String getTimestamp();
+
 }
