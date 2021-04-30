cae0a7eb53442620c41eb81a776ee26ce7c8e25a
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-30 23:31:31 +0200

    Added Docu and methods to IServer

--- src/main/java/de/throsenheim/vvss21/application/interfaces/IServer.java
+++ src/main/java/de/throsenheim/vvss21/application/interfaces/IServer.java
@@ -1,4 +1,22 @@
 package de.throsenheim.vvss21.application.interfaces;
 
+import de.throsenheim.vvss21.tcpserver.Connector;
+
+/**
+ * Interface for handling the accepting of new connections
+ */
 public interface IServer {
+
+    /**
+     * Method to remove a Connector from the Connector list in case the connection is closed
+     * @param connector Connector that should be removed
+     * @return True if operation was a success False if operation failed
+     */
+    boolean removeConnector(Connector connector);
+
+
+    /**
+     * Method to stop the Server and the connections
+     */
+    void stop();
 }
