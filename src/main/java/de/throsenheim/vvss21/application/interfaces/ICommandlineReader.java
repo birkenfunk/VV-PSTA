a75934f8bb7bbee981cdb3f15a3f5895e39bf64c
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-30 23:07:51 +0200

    Added Documentatin and methods to ICommandlineReader

--- src/main/java/de/throsenheim/vvss21/application/interfaces/ICommandlineReader.java
+++ src/main/java/de/throsenheim/vvss21/application/interfaces/ICommandlineReader.java
@@ -1,2 +1,14 @@
-package de.throsenheim.vvss21.application.interfaces;public interface ICommandlineReader {
+package de.throsenheim.vvss21.application.interfaces;
+
+/**
+ * Interface for reading from the commandline
+ * Can be used to read from the commandline
+ */
+public interface ICommandlineReader extends Runnable{
+
+    /**
+     * To stop the reading from the commandline
+     */
+    void stop();
+    
 }
