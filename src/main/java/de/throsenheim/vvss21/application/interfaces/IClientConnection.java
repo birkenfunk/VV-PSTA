14134918c8dcf35a7f11e181a49cb4243fd9cd79
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-30 23:02:01 +0200

    Added documentation and methods to IClientConnection

--- src/main/java/de/throsenheim/vvss21/application/interfaces/IClientConnection.java
+++ src/main/java/de/throsenheim/vvss21/application/interfaces/IClientConnection.java
@@ -1,2 +1,13 @@
-package de.throsenheim.vvss21.application.interfaces;public interface IClientConnection {
+package de.throsenheim.vvss21.application.interfaces;
+
+/**
+ * Interface for handling the client connection
+ * Can be used to send and receive data from the client
+ */
+public interface IClientConnection extends Runnable{
+
+    /**
+     * To terminate the connection with the client and end the Tread
+     */
+    void stop();
 }
