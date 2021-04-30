56d5df8a432a036efb3e17e7bd01c56435590163
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-30 23:27:15 +0200

    Added Docu and methods to the IMeasurementList

--- src/main/java/de/throsenheim/vvss21/application/interfaces/IMeasurementList.java
+++ src/main/java/de/throsenheim/vvss21/application/interfaces/IMeasurementList.java
@@ -1,2 +1,35 @@
-package de.throsenheim.vvss21.application.interfaces;public interface IMeasurementList {
+package de.throsenheim.vvss21.application.interfaces;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import de.throsenheim.vvss21.measurement.Measurement;
+
+import java.util.List;
+
+public interface IMeasurementList extends Runnable{
+
+    /**
+     * To get a {@link List} with {@link Measurement}
+     * @return List of {@link Measurement} Objects
+     */
+    public List<Measurement> getMeasurements();
+
+    /**
+     * Adds a {@link Measurement} to a blocking queue
+     * <p>If a thread is started the blocking queue will be emptied and added to the list
+     * @param measurement {@link Measurement} object that should be added
+     */
+    public void add(Measurement measurement);
+
+    /**
+     * Stops the thread and saves everything to a json file
+     */
+    public void stop();
+
+    /**
+     * Returns if the thread with the while loop is running
+     * @return false if thread is not running true if thread is running
+     */
+    @JsonIgnore
+    public boolean isRuning();
+
 }
