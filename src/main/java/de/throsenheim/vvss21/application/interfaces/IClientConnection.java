e8782bd99d05c3b28c902bd27114b19925c13299
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-30 23:06:15 +0200

    Added method is running to the IClientConnection

--- src/main/java/de/throsenheim/vvss21/application/interfaces/IClientConnection.java
+++ src/main/java/de/throsenheim/vvss21/application/interfaces/IClientConnection.java
@@ -10,4 +10,10 @@ public interface IClientConnection extends Runnable{
      * To terminate the connection with the client and end the Tread
      */
     void stop();
+
+    /**
+     * To check if the connection is still running
+     * @return True if connection is still active False if connection isn't active any more
+     */
+    boolean isRunning();
 }
