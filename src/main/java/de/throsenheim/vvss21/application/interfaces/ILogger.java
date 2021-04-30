ea531df00941d54fd4496e6b79ea356b40e8f31c
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-30 23:16:15 +0200

    Added docuementatin and methods to the ILogger

--- src/main/java/de/throsenheim/vvss21/application/interfaces/ILogger.java
+++ src/main/java/de/throsenheim/vvss21/application/interfaces/ILogger.java
@@ -1,2 +1,12 @@
-package de.throsenheim.vvss21.application.interfaces;public interface ILogger {
+package de.throsenheim.vvss21.application.interfaces;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Interface for logging data
+ * Can be used to add content to the Log
+ */
+public interface ILogger extends Logger {
+    void received(JsonNode node);
 }
