b3957913c73df1a274ee44b0d0b69ea2db6f3ce5
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-04-30 23:02:33 +0200

    Added Documentation and methods to IFileRead

--- src/main/java/de/throsenheim/vvss21/application/interfaces/IFileReader.java
+++ src/main/java/de/throsenheim/vvss21/application/interfaces/IFileReader.java
@@ -1,2 +1,26 @@
-package de.throsenheim.vvss21.application.interfaces;public interface IFileReader {
+package de.throsenheim.vvss21.application.interfaces;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * For handling the File reading
+ * Can be used to read a File
+ */
+public interface IFileReader {
+
+    /**
+     * Reads a file and puts the content into a String
+     * @param file witch file it should read
+     * @return String with the content of the file
+     */
+    String readFileToString(File file);
+
+    /**
+     * A method for reading a File
+     * @param file File that should be read
+     * @return Gives a List with the content of the File each Line ist a entry in the List
+     */
+    List<String> readFile(File file);
+
 }
