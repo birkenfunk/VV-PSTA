cbd400556e1ddfad5ed583d890f3201e0972286d
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-05-08 17:11:12 +0200

    Implimentation of Config class

--- src/main/java/de/throsenheim/vvss21/common/Config.java
+++ src/main/java/de/throsenheim/vvss21/common/Config.java
@@ -1,4 +1,32 @@
 package de.throsenheim.vvss21.common;
 
 public class Config {
+
+    private static final String DBUSERNAME = readDBUsername();
+    private static final String DBPASSWORD = readDBPassword();
+    private static final String DBURL = readDBURL();
+
+    private static String readDBPassword() {
+        return System.getenv().getOrDefault("DBPassword","1234");
+    }
+
+    private static String readDBURL() {
+        return System.getenv().getOrDefault("DBURL","localhost:3306/SmartHomeService");
+    }
+
+    private static String readDBUsername() {
+        return System.getenv().getOrDefault("DBUsername","Alex");
+    }
+
+    public static String getdBUsername() {
+        return DBUSERNAME;
+    }
+
+    public static String getdBPassword() {
+        return DBPASSWORD;
+    }
+
+    public static String getdBURL() {
+        return DBURL;
+    }
 }
