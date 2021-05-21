688f7f8df6122207edead974a27416f04e8cf1e9
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-05-21 12:58:10 +0200

    Added Spring boot loader to main and moved into package presentation

--- src/main/java/de/throsenheim/vvss21/presentation/Main.java
+++ src/main/java/de/throsenheim/vvss21/presentation/Main.java
@@ -1,14 +1,19 @@
-package de.throsenheim.vvss21;
+package de.throsenheim.vvss21.presentation;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
 
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
         LOGGER.info("Hello World");
     }
+
 
 }
