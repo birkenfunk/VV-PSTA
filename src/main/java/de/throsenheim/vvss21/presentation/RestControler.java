b0731d9d8a87e2bfb8599045e08e7d099a71acf1
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-05-24 17:03:57 +0200

    Hinzufügen von Funktions in den RestController
    Mappt die Dto klassen auf die DB Entitäten

--- src/main/java/de/throsenheim/vvss21/presentation/RestControler.java
+++ src/main/java/de/throsenheim/vvss21/presentation/RestControler.java
@@ -1,7 +1,7 @@
 package de.throsenheim.vvss21.presentation;
 
 import de.throsenheim.vvss21.application.interfaces.IDatabase;
-import de.throsenheim.vvss21.domain.Sensor;
+import de.throsenheim.vvss21.domain.*;
 import de.throsenheim.vvss21.persistence.MySQLConnector;
 import de.throsenheim.vvss21.persistence.exeptions.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.List;
+import java.util.function.Function;
 
 @RestController
 public class RestControler {
@@ -35,13 +35,13 @@ public class RestControler {
     }
 
     @PostMapping("/sensors")
-    public ResponseEntity createSensor(@RequestBody Sensor sensor){
-        try {
-            database.addSensor(sensor);
-            return ResponseEntity.ok(sensor);
-        }catch (IllegalArgumentException e){
+    public ResponseEntity createSensor(@RequestBody SensorDto sensor){
+        if(database.getSensor(sensor.getSensorId())!=null){
             return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
         }
+
+        database.addSensor(sensorDtoToSensor.apply(sensor));
+        return ResponseEntity.ok(sensor);
     }
 
     @DeleteMapping("/sensors/{id}")
@@ -55,4 +55,29 @@ public class RestControler {
             return ResponseEntity.status(HttpStatus.FAILED_DEPENDENCY).build();
         }
     }
+
+    Function<SensorDto, Sensor> sensorDtoToSensor = sensorDto -> new Sensor(sensorDto.getSensorId(),
+            sensorDto.getSensorName(),
+            sensorDto.getRegisterDate(),
+            sensorDto.getLocation()
+    );
+
+    Function<ActorDto, Actor> actorDtoToActor = actorDto -> new Actor(actorDto.getAktorId(),
+            actorDto.getAktorName(),
+            actorDto.getRegisterDate(),
+            actorDto.getLocation(),
+            actorDto.getServiceUrl(),
+            actorDto.getStatus());
+
+    Function<RuleDto, Rule> ruleDtoToRule = ruleDto -> new Rule(ruleDto.getRuleId(),
+            ruleDto.getRuleName(),
+            ruleDto.getTreshhold(),
+            database.getSensor(ruleDto.getSensorId()),
+            database.getActor(ruleDto.getAktorId()));
+
+    Function<SensorDataDto, SensorData> sensorDataDtoSensorToData = sensorDataDto -> new SensorData(sensorDataDto.getTemperaturUnit(),
+            sensorDataDto.getTimestamp(),
+            sensorDataDto.getCurrentValue(),
+            sensorDataDto.getSensorDataId(),
+            database.getSensor(sensorDataDto.getSensorId()));
 }
