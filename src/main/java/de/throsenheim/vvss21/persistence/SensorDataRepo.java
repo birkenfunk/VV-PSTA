ce769a99c1bd642ea09e869a05410099fc18d94d
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-05-29 19:29:39 +0200

    Hinzufügen von Spring Config zu ActorRepo, RuleRepo, SensorDataRepo

--- src/main/java/de/throsenheim/vvss21/persistence/SensorDataRepo.java
+++ src/main/java/de/throsenheim/vvss21/persistence/SensorDataRepo.java
@@ -1,7 +1,11 @@
 package de.throsenheim.vvss21.persistence;
 
 import de.throsenheim.vvss21.domain.entety.SensorData;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Configuration
+@ComponentScan("de.throsenheim.vvss21.domain.entety")
 public interface SensorDataRepo extends JpaRepository<SensorData, Integer> {
 }
