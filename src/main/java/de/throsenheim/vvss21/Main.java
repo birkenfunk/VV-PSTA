92beff8a660fa7a324b77caff8814cf8367c8e73
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-05-29 19:31:31 +0200

    Hinzufügen eines OpenApi return statements

--- src/main/java/de/throsenheim/vvss21/Main.java
+++ src/main/java/de/throsenheim/vvss21/Main.java
@@ -1,9 +1,14 @@
 package de.throsenheim.vvss21;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,5 +24,11 @@ public class Main {
         LOGGER.info("Hello World");
     }
 
+    @Bean
+    public OpenAPI customOpenAPI(@Value("3") String appVersion){
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Server API").version(appVersion));
+    }
 
 }
