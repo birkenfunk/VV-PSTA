c13ecfd24cab2d4b4779ec918474bc0dec5b152d
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-06-10 15:54:54 +0200

    Hinzufügen einer RuleEngine

---
+++ server/src/main/java/de/throsenheim/vvss21/application/RuleEngine.java
@@ -0,0 +1,52 @@
+package de.throsenheim.vvss21.application;
+
+import de.throsenheim.vvss21.domain.dtoentity.RuleDto;
+import de.throsenheim.vvss21.domain.dtoentity.SensorDataDto;
+
+import java.util.List;
+import java.util.TimerTask;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ */
+public class RuleEngine extends TimerTask {
+
+    private static final RuleEngine RULE_ENGINE = new RuleEngine();
+    private BlockingQueue<SensorDataDto> sensorDataDtos = new LinkedBlockingQueue<>();
+    private IDBConnector connector;
+
+    private RuleEngine(){
+
+    }
+
+    /**
+     * The action to be performed by this timer task.
+     */
+    @Override
+    public void run() {
+        int size = sensorDataDtos.remainingCapacity();
+        for (int i = 0 ; i<size; i++){
+            SensorDataDto data = null;
+            try {
+                 data = sensorDataDtos.poll(10, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+            List<RuleDto> rules = connector.getRulesForSensor(data.getSensorBySensorID().getSensorId());
+            //TODO Logic
+        }
+    }
+
+    public boolean addSensorData(SensorDataDto sensorDataDto){
+        sensorDataDtos.remove(sensorDataDto);
+        return sensorDataDtos.offer(sensorDataDto);
+    }
+
+    public static RuleEngine getRuleEngine() {
+        return RULE_ENGINE;
+    }
+}
