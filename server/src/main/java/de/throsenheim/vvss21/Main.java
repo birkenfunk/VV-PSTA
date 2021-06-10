d56e2ea84154e7c64bfb3104af89950668e987f3
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-06-10 18:21:48 +0200

    Starten eines Timers in Main

--- server/src/main/java/de/throsenheim/vvss21/Main.java
+++ server/src/main/java/de/throsenheim/vvss21/Main.java
@@ -6,10 +6,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 
 import java.util.Timer;
 
@@ -19,11 +21,18 @@ public class Main {
 
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
+    @Autowired
+    private RuleEngine ruleEngine;
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        Timer ruleTimer = new Timer();
-        ruleTimer.scheduleAtFixedRate(RuleEngine.getRuleEngine(),0, 30000);
         LOGGER.info("Hello World");
+    }
+
+    @Bean
+    public void startTimer(){
+        Timer ruleTimer = new Timer();
+        ruleTimer.scheduleAtFixedRate(ruleEngine,0, 30000);
     }
 
     @Bean
