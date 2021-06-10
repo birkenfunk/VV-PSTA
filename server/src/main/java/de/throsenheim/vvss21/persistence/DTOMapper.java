c0c60b3a0d4bb184e245d628af10892b1be76fd6
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2021-06-10 18:26:06 +0200

    Entfernen der Static Methoden des DTOMapper

--- server/src/main/java/de/throsenheim/vvss21/persistence/DTOMapper.java
+++ server/src/main/java/de/throsenheim/vvss21/persistence/DTOMapper.java
@@ -10,6 +10,7 @@ import de.throsenheim.vvss21.persistence.entety.Rule;
 import de.throsenheim.vvss21.persistence.entety.Sensor;
 import de.throsenheim.vvss21.persistence.entety.SensorData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
@@ -17,17 +18,14 @@ import java.util.function.Function;
 @Service
 public class DTOMapper {
 
+
     @Autowired
-    private static IDBConnector connector;
-
-    private DTOMapper(){
-
-    }
+    private IDBConnector connector;
 
     /**
      * Function for transforming a {@link SensorDto} Object to a {@link Sensor} object
      */
-    public static final Function<SensorDto, Sensor> sensorDtoToSensor = sensorDto -> new Sensor(sensorDto.getSensorId(),
+    public final Function<SensorDto, Sensor> sensorDtoToSensor = sensorDto -> new Sensor(sensorDto.getSensorId(),
             sensorDto.getSensorName(),
             sensorDto.getLocation()
     );
@@ -35,7 +33,7 @@ public class DTOMapper {
     /**
      * Function for transforming a {@link Sensor} Object to a {@link SensorDto} object
      */
-    public static final Function<Sensor, SensorDto> sensorToSensorDto = sensor -> new SensorDto(sensor.getSensorId(),
+    public final Function<Sensor, SensorDto> sensorToSensorDto = sensor -> new SensorDto(sensor.getSensorId(),
             sensor.getSensorName(),
             sensor.getLocation(),
             sensor.isDeleted()
@@ -44,7 +42,7 @@ public class DTOMapper {
     /**
      * Function for transforming a {@link SensorDataDto} Object to a {@link SensorData} object
      */
-    public static final Function<SensorDataDto, SensorData> sensorDataDtoSensorToData = sensorDataDto -> new SensorData(sensorDataDto.getTemperatureUnit(),
+    public final Function<SensorDataDto, SensorData> sensorDataDtoSensorToData = sensorDataDto -> new SensorData(sensorDataDto.getTemperatureUnit(),
             sensorDataDto.getTimestamp(),
             sensorDataDto.getCurrentValue(),
             sensorDtoToSensor.apply(sensorDataDto.getSensorBySensorID()));
@@ -52,7 +50,7 @@ public class DTOMapper {
     /**
      * Function for transforming a {@link SensorData} Object to a {@link SensorDataDto} object
      */
-    public static final Function<SensorData, SensorDataDto> sensorDataToSensorDataDto = sensorData -> new SensorDataDto(sensorData.getTemperatureUnit(),
+    public final Function<SensorData, SensorDataDto> sensorDataToSensorDataDto = sensorData -> new SensorDataDto(sensorData.getTemperatureUnit(),
             sensorData.getTimestamp(),
             sensorData.getCurrentValue(),
             sensorToSensorDto.apply(sensorData.getSensorBySensorId())
@@ -61,7 +59,7 @@ public class DTOMapper {
     /**
      * Function for transforming an {@link ActorDto} Object to a {@link Actor} object
      */
-    public static final Function<ActorDto, Actor> actorDtoToActor = actorDto -> new Actor(actorDto.getAktorId(),
+    public final Function<ActorDto, Actor> actorDtoToActor = actorDto -> new Actor(actorDto.getAktorId(),
             actorDto.getAktorName(),
             actorDto.getLocation(),
             actorDto.getServiceUrl(),
@@ -70,7 +68,7 @@ public class DTOMapper {
     /**
      * Function for transforming an {@link ActorDto} Object to an {@link Actor} object
      */
-    public static final Function<Actor, ActorDto> actorToActorDto = actor -> new ActorDto(actor.getAktorId(),
+    public final Function<Actor, ActorDto> actorToActorDto = actor -> new ActorDto(actor.getAktorId(),
             actor.getAktorName(),
             actor.getLocation(),
             actor.getServiceUrl(),
@@ -80,20 +78,21 @@ public class DTOMapper {
     /**
      * Function for transforming a {@link RuleDto} Object to a {@link Rule} object
      */
-    public static final Function<RuleDto, Rule> ruleDtoToRule = ruleDto -> new Rule(
-            ruleDto.getRuleName(),
-            ruleDto.getTreshhold(),
-            sensorDtoToSensor.apply(connector.getSensor(ruleDto.getSensorId())),
-            actorDtoToActor.apply(connector.getActor(ruleDto.getAktorId()))
+    public final Function<RuleDto, Rule> ruleDtoToRule = ruleDto ->  new Rule(
+                    ruleDto.getRuleName(),
+                    ruleDto.getTreshhold(),
+                    sensorDtoToSensor.apply(connector.getSensor(ruleDto.getSensorId())),
+                    actorDtoToActor.apply(connector.getActor(ruleDto.getActorId()))
     );
 
     /**
      * Function for transforming a {@link Rule} Object to a {@link RuleDto} object
      */
-    public static final Function<Rule, RuleDto> ruleToRuleDto = rule -> new RuleDto(
+    public final Function<Rule, RuleDto> ruleToRuleDto = rule -> new RuleDto(
             rule.getRuleName(),
             rule.getThreshold(),
             actorToActorDto.apply(rule.getActorByAktorId()),
             sensorToSensorDto.apply(rule.getSensorBySensorId())
     );
+
 }
